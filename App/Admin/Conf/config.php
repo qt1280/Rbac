534372c9e3b197871be91119b85aa616138d5db5
sunzhonghan <774878655@qq.com>
2016-07-09 18:47:42 +0800

    7-9下午第一次提交模块转移

--- App/Admin/Conf/config.php
+++ App/Admin/Conf/config.php
@@ -4,4 +4,8 @@ return array(
     'TMPL_ACTION_ERROR'      => THINK_PATH . 'Tpl/dispatch_jump.tpl', // 默认错误跳转对应的模板文件
     'TMPL_ACTION_SUCCESS'    => THINK_PATH . 'Tpl/dispatch_jump.tpl', // 默认成功跳转对应的模板文件
 
+    'TMPL_PARSE_STRING'=>array(
+        '__PUBLIC__'=>__ROOT__.'/App/Admin/public',
+    ),
+    
 );
